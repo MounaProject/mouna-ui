c27f08792f9042c8e8715cf39fb3409e70c307f4
KDBismael <ismaelkondombo1@gmail.com>
2022-10-27 08:19:45 +0000

    fixing hero section bugs

--- components/hero.vue
+++ components/hero.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="heroSection bg-[#fcedec] max-h-[800px] h-[calc(100vh-150px)] px-[5%] py-12 grid grid-cols-2 justify-center content-center">
+  <div class="heroSection bg-[#fcedec] max-h-[600px] h-[calc(100vh-150px)] px-[5%] py-12 grid grid-cols-2 justify-center content-center">
     <div class="text-center self-center">
       <div>
         <p class="font-medium uppercase my-4 text-xs text-lighter tracking-normal">choix de l'editeur</p>
@@ -14,19 +14,113 @@
       </div>
     </div>
     <div class="justify-self-center">
-      <div class="image w-full flex justify-center">
-        <img class="object-cover max-w-[460px] w-[60%]  h-auto" src="\books\photo_2022-09-17_18-09-43.jpg" alt="">
+      <div class="swiper">
+        <!-- <div class="swiper-wrapper w-full flex gap-x-4 justify-center">
+          <img class="object-cover max-w-[460px] w-[60%]  h-auto" src="\books\photo_2022-09-17_18-09-43.jpg" alt="">
+          <img class="swiper-slide object-cover w-[120px] rounded-lg hero-books-shadow h-auto" src="\books\photo_2022-09-17_18-09-43.jpg" alt="">
+          <img class="swiper-slide object-cover w-[120px] rounded-lg hero-books-shadow h-auto" src="\books\photo_2022-09-17_18-09-43.jpg" alt="">
+          <img class="swiper-slide object-cover w-[120px] rounded-lg hero-books-shadow h-auto" src="\books\photo_2022-09-17_18-09-43.jpg" alt="">
+        </div> -->
+        <client-only>
+          <slider class="swiper-wrapper w-full flex gap-x-4 justify-center" :indicators="false" :stopOnHover="true" :control-btn="false" height="280px">
+            <slider-item class="flex gap-x-4 justify-center">
+              <img class="swiper-slide  object-contain w-full rounded-lg h-full" src="\books\photo_2022-09-17_18-09-43.jpg" alt="">
+              <img class="swiper-slide  object-contain w-full rounded-lg h-full" src="\books\photo_2022-09-17_18-09-43.jpg" alt="">
+            </slider-item>
+            <slider-item class="flex gap-x-4 justify-center">
+              <img class="swiper-slide  object-contain w-full rounded-lg h-full" src="books/photo_2022-09-17_18-10-12.jpg" alt="">
+              <img class="swiper-slide  object-contain w-full rounded-lg h-full" src="books/photo_2022-09-17_18-10-12.jpg" alt="">
+            </slider-item>
+            <slider-item class="flex gap-x-4 justify-center">
+              <img class="swiper-slide  object-contain w-full rounded-lg h-full" src="\books\photo_2022-09-17_18-09-43.jpg" alt="">
+              <img class="swiper-slide  object-contain w-full rounded-lg h-full" src="\books\photo_2022-09-17_18-09-43.jpg" alt="">
+            </slider-item>
+            <slider-item class="flex gap-x-4 justify-center">
+              <img class="swiper-slide  object-contain w-full rounded-lg h-full" src="books/photo_2022-09-17_18-11-34.jpg" alt="">
+              <img class="swiper-slide  object-contain w-full rounded-lg h-full" src="books/photo_2022-09-17_18-11-34.jpg" alt="">
+            </slider-item>
+          </slider>
+        </client-only>
+        <!-- <client-only>
+            <slider animation="fade">
+              <slider-item
+                v-for="(i, index) in list"
+                :key="index"
+                :style="i"
+              >
+                <p style="line-height: 280px; font-size: 5rem; text-align: center;">Page{{ index + 1 }}</p>
+              </slider-item>
+            </slider>
+        </client-only> -->
+        <img src="/stand.png" alt="">
       </div>
     </div>
   </div>
 </template>
 
 <script>
+import  { Swiper, Navigation, Pagination, EffectCards, Autoplay } from 'swiper';
+// import 'swiper/swiper-bundle.min.css'
+import Vue from 'vue'
+import EasySlider from 'vue-easy-slider'
+
+Vue.use(EasySlider)
+
 export default {
   name:'hero',
+  data() {
+    return {
+      list: [
+        { backgroundColor: '#3f51b5', width: '100%', height: '100%' },
+        { backgroundColor: '#eee', width: '100%', height: '100%' },
+        { backgroundColor: '#f44336', width: '100%', height: '100%' },
+      ],
+    }
+  },
+  // mounted() {
+  //   // configure Swiper to use modules. The modules were tested with SwiperJS v6.8.4 with NuxtJS v2.15.7
+  //   // previously it was before export default. Moved here for performance issues. Move back in case of problems.
+  //   // init Swiper:  eslint-disable used for deleting error of unsued const swiper
+  //   /* eslint-disable no-unused-vars */
+  //   const swiper = new Swiper('.swiper', {
+  //     // Optional parameters
+  //     // @see https://swiperjs.com/swiper-api#parameters
+  //     loop: false,
+  //     effect: "cards",
+  //     grabCursor: true,
+  //     // remove unused modules if needed
+  //     modules: [Navigation, Pagination, EffectCards, Autoplay],
+  //     // Pagination if needed
+  //     pagination: {
+  //       el: '.swiper-pagination',
+  //       type: 'bullets',
+  //       clickable: true,
+  //     },
+  //     // Autoplay if needed
+  //     autoplay: {
+  //       delay: 1500,
+  //     },
+  //     // Navigation arrows if needed
+  //     navigation: {
+  //       nextEl: '.swiper-button-next',
+  //       prevEl: '.swiper-button-prev',
+  //     },
+  //   })
+  //   // you can use different options later
+  //   swiper.on('activeIndexChange', (swiper) => {
+  //     // console.log(swiper)
+  //   })
+  // },
 }
 </script>
 
-<style>
-
+<style scoped>
+  .hero-books-shadow{
+    box-shadow: grey -3px 3px 10px 0px;
+    margin-bottom: 2px;
+    z-index: 1;
+  }
+  .swiper-slide {
+    /* width: 120px !important; */
+}
 </style>
